271f6ae65a93cd1c9464245da9ab7259eef187b5
a7xnico <pablonicolasarayamaza@gmail.com>
2025-05-19 14:29:56 -0300

    Add files via upload

--- TP7 archivos/main.c
+++ TP7 archivos/main.c
@@ -117,7 +117,7 @@ int main()
     int alumnoBuscado = pedirNumeroAlumno();
     verificacionCantAlumnos(archivoAlumnos);
     mostrarAlumnoBuscado(archivoAlumnos, alumnoBuscado);
-    printf("\nIngrese el numero del alumno que quiera sobreescribir: \n");
+    printf("Ingrese el numero del alumno que quiera sobreescribir: \n");
     int alumnoIntercambiado = pedirNumeroAlumno();
     sobreescribirAlumno(archivoAlumnos, alumnoIntercambiado);
 }
@@ -639,7 +639,7 @@ void verificacionCantAlumnos(char nombreArchivo[])
     }
 }
 
-void sobreescribirAlumnoEntero(char nombreArchivo[], int numAlumno)
+void sobreescribirAlumno(char nombreArchivo[], int numAlumno)
 {
     FILE *fp;
     stAlumno a;
@@ -654,37 +654,7 @@ void sobreescribirAlumnoEntero(char nombreArchivo[], int numAlumno)
         printf("\nERROR: No pudo abrirse el archivo");
 }
 
-void sobreescribirAlumno(char nombreArchivo[], int numAlumno)
-{
-    int opcion = 0;
-    do
-    {
-        printf("\nIngrese el numero que quiera intercambiar: \n");
-        printf("1-Legajo del alumno\n");
-        printf("2-Nombre del alumno\n");
-        printf("3-Edad del alumno\n");
-        printf("4-Anio del alumno\n");
-        printf("5-El alumno entero\n");
-        scanf("%d", &opcion);
-    }while(opcion < 1 || opcion > 5);
-    switch(opcion)
-    {
-    case 1:
 
-
-
-
-
-
-
-
-    case 5:
-        {
-            sobreescribirAlumnoEntero(nombreArchivo, numAlumno);
-        }
-    }
-
-}
 
 
 
