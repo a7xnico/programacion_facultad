51697ef3999dc12858071e8bb88c65192f93dec0
a7xnico <pablonicolasarayamaza@gmail.com>
2025-06-02 15:00:11 -0300

    Add files via upload

--- simulacro 2do parcial/main.c
+++ simulacro 2do parcial/main.c
@@ -22,6 +22,17 @@ typedef struct
 	int valPlanetas;
 }galaxia;
 
+planeta cargarPlaneta();
+
+int cargarPlanetas(planeta planetas[], int dimPlanetas);
+
+galaxia cargarGalaxia();
+
+int cargarGalaxias(galaxia galaxias[]);
+
+void mostrarGalaxias(galaxia galaxias[], int val, char tipo[20]);
+
+
 int main()
 {
     galaxia galaxias[cantGalaxias];
@@ -35,26 +46,26 @@ planeta cargarPlaneta()
 
     printf("Ingresar el nombre del planeta: ");
     fgets(p.nombrePlaneta, 20, stdin);
-    p.nombrePlaneta[strcspn(p.nombrePlaneta, '\n')] = 0;
+    p.nombrePlaneta[strcspn(p.nombrePlaneta, "\n")] = 0;
 
     printf("Ingresar la estrella de este planeta: ");
     fgets(p.estrella, 20, stdin);
-    p.estrella[strcspn(p.estrella, '\n')] = 0;
+    p.estrella[strcspn(p.estrella, "\n")] = 0;
 
     printf("Ingresar la cantidad de satelites del planeta: ");
     scanf("%d", &p.cantidadSatelites);
     while(getchar() != '\n');
 
     printf("Ingresar la masa del planeta en relacion a la tierra: ");
-    scanf("%f", p.masa);
+    scanf("%f", &p.masa);
     while(getchar() != '\n');
 
     printf("Ingresar la temperatura maxima del planeta: ");
-    scanf("%d", p.tempMax);
+    scanf("%d", &p.tempMax);
     while(getchar() != '\n');
 
     printf("Ingresar la temperatura minima: ");
-    scanf("%d", p.tempMin);
+    scanf("%d", &p.tempMin);
     while(getchar() != '\n');
 
     return p;
@@ -62,7 +73,7 @@ planeta cargarPlaneta()
 
 int cargarPlanetas(planeta planetas[], int dimPlanetas)
 {
-    int seguir;
+    int seguir = 0;
     int val = 0;
     do
     {
@@ -70,7 +81,8 @@ int cargarPlanetas(planeta planetas[], int dimPlanetas)
         planetas[val] = cargarPlaneta();
         val++;
         printf("Para cargar otro planeta presione 1: ");
-        scanf("%d", seguir);
+        scanf("%d", &seguir);
+        while(getchar() != '\n');
     }while(seguir == 1 && val < dimPlanetas);
     return val;
 }
@@ -81,15 +93,15 @@ galaxia cargarGalaxia()
 
     printf("Ingresar el nombre de la galaxia: ");
     fgets(g.nombreGalaxia, 20, stdin);
-    g.nombreGalaxia[strcspn(g.nombreGalaxia, '\n')] = 0;
+    g.nombreGalaxia[strcspn(g.nombreGalaxia, "\n")] = 0;
 
     printf("Ingresar el tipo de galaxia: ");
     fgets(g.tipoGalaxia, 20, stdin);
-    g.tipoGalaxia[strcspn(g.tipoGalaxia, '\n')] = 0;
+    g.tipoGalaxia[strcspn(g.tipoGalaxia, "\n")] = 0;
 
     printf("Ingresar por quien fue descubierta: ");
     fgets(g.descubiertoPor, 20, stdin);
-    g.descubiertoPor[strcspn(g.descubiertoPor, '\n')] = 0;
+    g.descubiertoPor[strcspn(g.descubiertoPor, "\n")] = 0;
 
     g.valPlanetas = cargarPlanetas(g.listaPlanetas, 20);
 
@@ -98,24 +110,112 @@ galaxia cargarGalaxia()
 
 int cargarGalaxias(galaxia galaxias[])
 {
-    int seguir, val = 0;
+    int seguir = 1, val = 0;
     do
     {
         printf("Galaxia numero %d:\n", val+1);
         galaxias[val] = cargarGalaxia();
         val++;
         printf("Para cargar otra galaxia presione 1: ");
-        scanf("%d", seguir);
+        scanf("%d", &seguir);
+        while(getchar() != '\n');
     }while(seguir == 1 && val < cantGalaxias);
     return val;
 }
 
+void mostrarPlaneta(planeta p)
+{
+    printf("Nombre del planeta: %s\n", p.nombrePlaneta);
+    printf("Estrella del planeta: %s\n", p.estrella);
+    printf("Cantidad de satelites: %d\n", p.cantidadSatelites);
+    printf("Temperaturas MAX: %d  MIN: %d\n", p.tempMax, p.tempMin);
+    printf("Masa del planeta: %.2f\n", p.masa);
+}
 
+void mostrarPlanetas(planeta p[], int val)
+{
+    for(int i = 0; i < val; i++)
+    {
+        printf("\n-----PLANETA %d-----\n", i+1);
+        mostrarPlaneta(p[i]);
+    }
+}
 
+void mostrarGalaxia(galaxia g)
+{
+    printf("\n\n=========%s=========\n\n", g.nombreGalaxia);
+    printf("Descubierto por: %s\n", g.descubiertoPor);
+    printf("Galaxia tipo: %s\n", g.tipoGalaxia);
+    mostrarPlanetas(g.listaPlanetas, g.valPlanetas);
+}
 
+void mostrarGalaxias(galaxia galaxias[], int val, char tipoBuscado[20])
+{
+    for(int i = 0; i < val; i++)
+        if(strcasecmp(galaxias[i].tipoGalaxia, tipoBuscado) == 0)
+            mostrarGalaxia(galaxias[i]);
+}
 
+int cantidadSatelitesTotal(galaxia galaxias[], int valGalaxias, int i)
+{
+    int satelites = cantidadSatelitesGalaxia(galaxias[i], galaxias[i].valPlanetas, i);
+    if(i < valGalaxias - 1)
+        satelites += cantidadSatelitesTotal(galaxias, valGalaxias, i+1);
+    return satelites;
+}
 
+int cantidadSatelitesGalaxia(galaxia g, int valPlanetas, int i)
+{
+    int satelites = g.listaPlanetas[i].cantidadSatelites;
+    if(i < valPlanetas - 1)
+        satelites += cantidadSatelitesGalaxia(g, valPlanetas, i+1);
+    return satelites;
+}
 
+void guardarArregloPlanetas(char nombreArchivo[], planeta planetas[], int valPlanetas, float masaMinima)
+{
+    FILE* fp;
+    int i = 0;
+    planeta a;
+    fp = fopen(nombreArchivo, "ab");
+    if(fp)
+    {
+        while(i < valPlanetas)
+        {
+            a = planetas[i];
+            if(a.masa >= masaMinima)
+                fwrite(&a, sizeof(planeta), 1, fp);
+            i++;
+        }
+        fclose(fp);
+    }
+    else
+        printf("\nERROR: No pudo abrirse el archivo.\n");
+}
 
-
-
+int ArregloDinamicoPlanetas(char nombreArchivo[], planeta** arrDinamico)
+{
+    FILE* fp;
+    fp = fopen(nombreArchivo, "rb");
+    int cantPlanetas = 0;
+    int i = 0;
+    if(fp)
+    {
+        fseek(fp, 0 , SEEK_END);
+        cantPlanetas = (ftell(fp) / sizeof(planeta));
+        fclose(fp);
+    }
+    *arrDinamico = malloc(cantPlanetas * sizeof(planeta));
+    fp = fopen(nombreArchivo, "rb");
+    if(fp)
+    {
+        planeta a;
+        while(fread(&a, sizeof(planeta), 1, fp))
+        {
+            (*arrDinamico)[i] = a;
+            i++;
+        }
+        fclose(fp);
+    }
+    return i;
+}
